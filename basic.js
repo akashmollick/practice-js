33465675299747d0afdb9efda6a93c56a35ea5e6
akashmollick <akashmollick88@gmail.com>
2023-01-31 09:11:58 +0800

    h

--- basic.js
+++ basic.js
@@ -1,1 +1,32 @@
-console.log("hello brother")
+var orange=30;
+var dim=40;
+var total=orange*3+dim*5
+console.log(total);
+// upadate new thing or varible 
+var hello =30;
+var hello=hello+20;
+ hello+=20;
+ hello++
+ var joyBangla="hello world"
+console.log(hello);
+console.log(typeof joyBangla )
+// array
+var arrayFriend=[12,34,4,4,55,5,555,5];
+console.log(arrayFriend.length)
+// while loop
+var roast=0;
+// while(roast<=8){
+//     console.log("vai ami chill");
+//     console.log(roast);
+//     roast++;
+// }
+// even loop
+// var number=1;
+// while(number <=10){
+//     console.log(number);
+//     number+=2;
+// }
+for(var i=0;i<=8;i++){
+    console.log(i);
+}
+
